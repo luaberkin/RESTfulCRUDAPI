e2d05b86c099d18954f8bea948714433fd1f8d97
Alua Berkin <alua.berkin@gmail.com>
2021-06-09 10:01:22 +0600

    My first Spring Boot project with PostgreSQL database

---
+++ src/main/java/com/example/postgresdemo/model/Answer.java
@@ -0,0 +1,74 @@
+package com.example.postgresdemo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "answers")
+public class Answer extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "answer_generator")
+    @SequenceGenerator(
+            name = "answer_generator",
+            sequenceName = "answer_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "question_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Question question;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Date getCreatedAt() {
+        return super.getCreatedAt();
+    }
+
+    @Override
+    public void setCreatedAt(Date createdAt) {
+        super.setCreatedAt(createdAt);
+    }
+
+    @Override
+    public Date getUpdatedAt() {
+        return super.getUpdatedAt();
+    }
+
+    @Override
+    public void setUpdatedAt(Date updatedAt) {
+        super.setUpdatedAt(updatedAt);
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/com/example/postgresdemo/model/Question.java
@@ -0,0 +1,45 @@
+package com.example.postgresdemo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "questions")
+public class Question extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "question_generator")
+    @SequenceGenerator(
+            name = "question_generator",
+            sequenceName = "question_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
